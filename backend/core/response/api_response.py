124c451a2876834f40d14978a41b1e0c583db6b5
PhamThi1710 <92194681+PhamThi1710@users.noreply.github.com>
2024-12-07 14:33:23 +0700

    refactor: simplify Ok and Error response classes by using default attributes

--- backend/core/response/api_response.py
+++ backend/core/response/api_response.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 
 T = TypeVar("T")
 
-
 class BaseResponse(BaseModel, Generic[T]):
     isSuccess: bool
     errorCode: int
@@ -13,22 +12,20 @@ class BaseResponse(BaseModel, Generic[T]):
 
 
 class Ok(BaseResponse[T]):
-    def __init__(self, data: T, message: str = "Operation successful", message_code: str = "SUCCESS"):
-        super().__init__(
-            isSuccess=True,
-            errorCode=0,
-            message=message,
-            messageCode=message_code,
-            data=data
-        )
+    isSuccess: bool = True
+    errorCode: int = 0
+    message: str = "Operation successful"
+    messageCode: str = "SUCCESS"
+
+    def __init__(self, data: T, **kwargs):
+        super().__init__(data=data, **kwargs)
 
 
 class Error(BaseResponse[None]):
-    def __init__(self, error_code: int, message: str, message_code: str = "ERROR"):
-        super().__init__(
-            isSuccess=False,
-            errorCode=error_code,
-            message=message,
-            messageCode=message_code,
-            data=None
-        )
+    isSuccess: bool = False
+    errorCode: int
+    message: str
+    messageCode: str = "ERROR"
+
+    def __init__(self, error_code: int, message: str, **kwargs):
+        super().__init__(errorCode=error_code, message=message, **kwargs)
