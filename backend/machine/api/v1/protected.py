1e96ae809ff54df03501eb2ae9c9cdf0bcd9b7ce
PhamThi1710 <92194681+PhamThi1710@users.noreply.github.com>
2025-01-07 23:50:21 +0700

    feat: add token verification utility and refactor protected route to use it

--- backend/machine/api/v1/protected.py
+++ backend/machine/api/v1/protected.py
@@ -1,47 +1,26 @@
-import os
-import jwt
-from jwt import PyJWTError
-from datetime import datetime
-from dotenv import load_dotenv
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import APIRouter, Depends, HTTPException, Header
-from datetime import datetime, timedelta, timezone
+from core.utils.auth_utils import verify_token
 
-load_dotenv()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 router = APIRouter(prefix="/auth", tags=["auth"])
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-
-
-def verify_token(token: str) -> dict:
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-
-        exp = payload.get("exp")
-        current_time = int(datetime.now(timezone(timedelta(hours=7))).timestamp())
-
-        if exp is None:
-            raise HTTPException(status_code=401, detail="Token has no expiration")
-
-        if current_time > exp:
-            raise HTTPException(status_code=401, detail="Token has expired")
-
-        return payload
-
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
-    except PyJWTError as e:  
-        raise HTTPException(status_code=401, detail=f"Invalid token: {str(e)}")
-
 
 @router.get("/protected")
 async def protected_route(token: str = Depends(oauth2_scheme)):
     """
     Test Protected Route with Bearer Token
     This endpoint is protected, so only users with valid tokens can access it.
+    Example of payload:
+    {
+    "message": "Token is valid",
+    "user_info": {
+        "sub": "8fb9b551-d176-4966-bc6b-f52fdfca14c7" => This is the id of the user, 
+        "exp": 1736269971
+        }
+    }
     """
+    
+    
     payload = verify_token(token)
-
     return {"message": "Token is valid", "user_info": payload}
--- backend/machine/api/v1/user.py
+++ backend/machine/api/v1/user.py
@@ -8,6 +8,7 @@
 # from machine.providers import InternalProvider
 # from machine.schemas.requests.user import UserRequest
 # from machine.schemas.responses.user import UserResponse
+# from machine.api.v1.protected import verify_token
 
 # router = APIRouter(prefix="/users", tags=["users"])
 
@@ -33,44 +34,44 @@
 #         return created_user
 
 
-# @router.post("/upsert")
-# async def upsert(
-#     body: Union[UserRequest, List[UserRequest]],
-#     bulk: bool = Query(False, description="Whether to upsert a bulk of users"),
-#     user_controller: UserController = Depends(InternalProvider().get_user_controller),
-# ):
-#     """
-#     Upsert user(s)
-#     """
-#     if bulk:
-#         if not isinstance(body, List):
-#             raise BadRequestException("Body must be a list when bulk is True")
-#         created_users = await user_controller.upsert_many(index_elements=["id"], attributes_list=[user.model_dump() for user in body])
-#         return created_users
-#     else:
-#         if isinstance(body, List):
-#             raise BadRequestException("Body must be a single user when bulk is False")
-#         created_user = await user_controller.upsert(index_elements=["id"], attributes=body.model_dump())
-#         return created_user
+# # @router.post("/upsert")
+# # async def upsert(
+# #     body: Union[UserRequest, List[UserRequest]],
+# #     bulk: bool = Query(False, description="Whether to upsert a bulk of users"),
+# #     user_controller: UserController = Depends(InternalProvider().get_user_controller),
+# # ):
+# #     """
+# #     Upsert user(s)
+# #     """
+# #     if bulk:
+# #         if not isinstance(body, List):
+# #             raise BadRequestException("Body must be a list when bulk is True")
+# #         created_users = await user_controller.upsert_many(index_elements=["id"], attributes_list=[user.model_dump() for user in body])
+# #         return created_users
+# #     else:
+# #         if isinstance(body, List):
+# #             raise BadRequestException("Body must be a single user when bulk is False")
+# #         created_user = await user_controller.upsert(index_elements=["id"], attributes=body.model_dump())
+# #         return created_user
 
 
-# @router.get("/")
-# async def list(
-#     user_controller: UserController = Depends(InternalProvider().get_user_controller),
-# ):
-#     """
-#     List all users
-#     """
-#     users = await user_controller.get_many(distinct=[User.name])
-#     return users
+# # @router.get("/")
+# # async def list(
+# #     user_controller: UserController = Depends(InternalProvider().get_user_controller),
+# # ):
+# #     """
+# #     List all users
+# #     """
+# #     users = await user_controller.get_many(distinct=[User.name])
+# #     return users
 
 
-# @router.delete("/{id}")
-# async def delete(
-#     id: int,
-#     user_controller: UserController = Depends(InternalProvider().get_user_controller),
-# ):
-#     """
-#     Delete a user
-#     """
-#     return await user_controller.delete(where_=[User.id == id])
+# # @router.delete("/{id}")
+# # async def delete(
+# #     id: int,
+# #     user_controller: UserController = Depends(InternalProvider().get_user_controller),
+# # ):
+# #     """
+# #     Delete a user
+# #     """
+# #     return await user_controller.delete(where_=[User.id == id])
