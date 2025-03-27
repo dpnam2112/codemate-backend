965eec71005ed7d6d66547ecc688dc0a6215eb58
Thi <lsjenny1710@gmail.com>
2025-03-27 17:46:23 +0700

    feat: update student courses controller dependency and enhance lesson creation order logic

--- machine/api/v1/professor.py
+++ machine/api/v1/professor.py
@@ -54,7 +54,8 @@ async def get_professor_courses(
 
     # If no courses are found
     if not courses:
-        return Ok(data=[], message="No courses found.")
+        return Ok(data=GetProfessorCoursesPaginatedResponse(content=[], currentPage=page, pageSize=page_size, totalRows=0, totalPages=0), message="No courses found.")
+
 
     courses_response = []
     for course in courses:
