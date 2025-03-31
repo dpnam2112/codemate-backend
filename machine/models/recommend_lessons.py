2873c6eebcb9a040bdb011233319784fa30f2657
Thi <lsjenny1710@gmail.com>
2025-03-31 12:25:03 +0700

    feat: add time spent tracking for recommended lessons and enhance lesson DTO

--- machine/models/recommend_lessons.py
+++ machine/models/recommend_lessons.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, Text, Boolean, ForeignKey, Enum
+from sqlalchemy import Column, Integer, Text, Boolean, ForeignKey, Enum, Interval
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
 from sqlalchemy.orm import relationship
 from core.db import Base
 from core.repository.enum import StatusType
+from datetime import timedelta
 
 
 class RecommendLessons(Base):
@@ -21,6 +22,7 @@ class RecommendLessons(Base):
     end_date = Column(Text, nullable=True)
     duration_notes=Column(Text, nullable=True)
     order = Column(Integer, default=0, nullable=True)
+    time_spent = Column(Interval, default=timedelta(0), nullable=True) 
 
     lesson = relationship("Lessons", back_populates="recommend_lesson")
     learning_path = relationship("LearningPaths", back_populates="recommend_lessons")
